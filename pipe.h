37477362462b5e0cc4898126ba24c4fe7e4a04d5
AbdalrhmanJuber <131310321+AbdalrhmanJuber@users.noreply.github.com>
2025-03-25 20:51:08 +0200

    Update pipe.h

--- pipe.h
+++ pipe.h
@@ -1,13 +1,14 @@
-// pipe.h
 #ifndef PIPE_H
 #define PIPE_H
 
-#define MAX_PLAYERS 8
+// For each player, we have a pipe for child -> parent communication
+// fds[i][0] is read end, fds[i][1] is write end
+int init_pipes(int fds[][2], int n);
 
-// A pair of fds per player: fds[i][0] = read, fds[i][1] = write
-int init_pipes(int fds[MAX_PLAYERS][2]);
-int close_pipe_ends(int fds[2], int read, int write);
-int send_to_parent(int write_fd, int value);
-int read_from_child(int read_fd);
+// read from child => returns number of bytes read
+int read_effort(int read_fd, void *buffer, int buf_size);
+
+// write from child => returns number of bytes written
+int write_effort(int write_fd, void *buffer, int buf_size);
 
 #endif
