e334d5348e9443774cc53ed7d2e378e56c951c23
AbdalrhmanJuber <131310321+AbdalrhmanJuber@users.noreply.github.com>
2025-03-25 20:49:16 +0200

    Update and rename config.txt to config.h

---
+++ config.h
@@ -0,0 +1,22 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+typedef struct {
+    int energy_min;
+    int energy_max;
+
+    int decay_min;
+    int decay_max;
+
+    int fall_recover_min;
+    int fall_recover_max;
+
+    int win_threshold;
+    int max_game_time;      // in seconds
+    int max_score;
+    int consecutive_wins;
+} GameConfig;
+
+int load_config(const char *filename, GameConfig *cfg);
+
+#endif
