bf5472ef73836d01b7400978b18a201fa03b901f
AbdalrhmanJuber <131310321+AbdalrhmanJuber@users.noreply.github.com>
2025-03-25 20:54:38 +0200

    Update player.c

--- player.c
+++ player.c
@@ -5,8 +5,7 @@
 #include <time.h>
 
 #include "comm.h"
-#include "pipe.h"     // if using pipes
-// #include "fifo.h"   // if you prefer named FIFOs
+#include "pipe.h"
 #include "config.h"
 
 static PlayerData me;   // Info about this player
@@ -14,34 +13,65 @@ static int write_fd = -1;  // For sending EffortMessage to referee
 
 // Called when we get SIG_READY
 void on_ready(int sig) {
-    // TODO: handle alignment logic
-    // e.g. reorder location based on who has highest energy
-    // For now, just print
-    printf("[Player %d, Team %d] Received SIG_READY\n", me.id, me.team);
+    // Naive approach: reorder location based on local energy,
+    // but we don't know teammates' energies, so let's just randomize location.
+    me.location = rand() % 4;
+    printf("[Player %d, Team %d] SIG_READY => location set to %d (energy=%d)\n",
+           me.id, me.team, me.location, me.energy);
+
+    // If we had actual team comparison, we'd gather all players' energies, sort, etc.
 }
 
 // Called when we get SIG_PULL
 void on_pull(int sig) {
-    // TODO: start pulling. Possibly start a timer or set a flag
-    printf("[Player %d, Team %d] Received SIG_PULL\n", me.id, me.team);
+    // Start pulling => might do nothing but let's just log
+    printf("[Player %d, Team %d] SIG_PULL => Start pulling\n", me.id, me.team);
 }
 
-// Example: simulate each second
-void do_one_second_of_play() {
-    // Decrement energy
-    if (!me.is_fallen) {
+// Called when we get SIG_TERMINATE
+void on_terminate(int sig) {
+    printf("[Player %d, Team %d] Terminating...\n", me.id, me.team);
+    exit(0);
+}
+
+// Each second, do pulling logic
+void do_one_second_of_play()
+{
+    // If we are fallen, decrement fall_time_left
+    if (me.is_fallen) {
+        me.fall_time_left--;
+        if (me.fall_time_left <= 0) {
+            // Recovered
+            me.is_fallen = 0;
+            // Let's assume we rejoin with random energy between 1..(current + 10)
+            // or you can set it to some fraction of old energy.
+            int regained = (rand() % 10) + 1;
+            me.energy += regained;
+            printf("[Player %d, Team %d] Recovered from fall! New energy=%d\n", me.id, me.team, me.energy);
+        }
+    } else {
+        // Decrement energy only if not fallen
         me.energy -= me.decay_rate;
         if (me.energy < 0) me.energy = 0;
+
+        // Random chance to fall: e.g. 10%
+        int r = rand() % 100;
+        if (r < 10) { // 10% chance
+            me.is_fallen = 1;
+            me.energy = 0;
+            // random recovery time in [fall_recover_min..fall_recover_max]
+            int min_recover = 2; // Hardcode or store from config
+            int max_recover = 5; // Hardcode or store from config
+            // For a more robust approach, you'd pass fall recover range to child 
+            // just like we pass energy/decay, but for brevity let's hardcode or guess
+            me.fall_time_left = (rand() % (max_recover - min_recover + 1)) + min_recover;
+            printf("[Player %d, Team %d] Fell! Will recover in %d secs\n", me.id, me.team, me.fall_time_left);
+        }
     }
 
-    // Possibly check if we "fell"
-    // TODO: random chance
-    // if fell, me.is_fallen = 1 => me.energy = 0 => rejoin after random time
-
-    // Weighted effort = energy * (1 + location),
-    // e.g. location=0 => multiplier=1, location=1 => mult=2, etc.
+    // Weighted effort => energy * (1 + location)
     int multiplier = 1 + me.location;
-    int weighted = me.energy * multiplier;
+    int weighted = me.is_fallen ? 0 : (me.energy * multiplier);
 
     // Send EffortMessage to parent
     EffortMessage msg;
@@ -57,25 +87,25 @@ int main(int argc, char *argv[])
     srand(time(NULL) ^ getpid());
 
     // Expect arguments: [0]=binary, [1]=id, [2]=team, [3]=decay, [4]=init_energy, [5]=write_fd
-    me.id       = atoi(argv[1]);
-    me.team     = atoi(argv[2]);
-    me.decay_rate = atoi(argv[3]);
-    me.energy   = atoi(argv[4]);
-    write_fd    = atoi(argv[5]);
+    me.id          = atoi(argv[1]);
+    me.team        = atoi(argv[2]);
+    me.decay_rate  = atoi(argv[3]);
+    me.energy      = atoi(argv[4]);
+    write_fd       = atoi(argv[5]);
 
-    me.is_fallen = 0;
-    me.location  = me.id; // naive: location = id
+    me.is_fallen   = 0;
+    me.location    = me.id; 
+    me.fall_time_left = 0;
 
     // Register signal handlers
     signal(SIG_READY, on_ready);
     signal(SIG_PULL, on_pull);
+    signal(SIG_TERMINATE, on_terminate);
 
-    // Each second, do some logic
+    // Infinite loop: each second simulate
     while (1) {
-        // Could wait for a small period
         sleep(1);
         do_one_second_of_play();
     }
-
     return 0;
 }
